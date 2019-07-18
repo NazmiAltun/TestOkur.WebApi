a0c84631ea96adb4e25436885309048e9d43984c
NAZMI <NAZMI@DESKTOP-20STISF>
2019-07-18 07:40:30 +0300

    Rework : User seeder

--- src/TestOkur.WebApi/Data/UserSeeder.cs
+++ src/TestOkur.WebApi/Data/UserSeeder.cs
@@ -33,9 +33,9 @@
 					.ToList();
 				for (var i = 2; i < workSheet.Dimension.Rows + 1; i++)
 				{
-					var city = cities.First(c => c.Id == Convert.ToInt32(workSheet.Cells[i, 20].Value));
-					var district = city.Districts.First(d => d.Id == Convert.ToInt32(workSheet.Cells[i, 18].Value));
-					var smsBalance = Convert.ToInt32(workSheet.Cells[i, 17].Value);
+					var city = cities.First(c => c.Id == Convert.ToInt32(workSheet.Cells[i, 2].Value));
+					var district = city.Districts.First(d => d.Id == Convert.ToInt32(workSheet.Cells[i, 3].Value));
+					var smsBalance = Convert.ToInt32(workSheet.Cells[i, 11].Value);
 
 					list.Add(ParseFromRow(workSheet, i, city, district));
 
@@ -53,16 +53,16 @@
 		private User ParseFromRow(ExcelWorksheet workSheet, int rowIndex, City city, District district)
 		{
 			return new User(
-				workSheet.Cells[rowIndex, 29].Value.ToString(),
+				workSheet.Cells[rowIndex, 1].Value.ToString(),
 				city,
 				district,
-				workSheet.Cells[rowIndex, 6].Value.ToString(),
-				workSheet.Cells[rowIndex, 7].Value.ToString(),
-				workSheet.Cells[rowIndex, 3].Value.ToString(),
 				workSheet.Cells[rowIndex, 4].Value.ToString(),
 				workSheet.Cells[rowIndex, 5].Value.ToString(),
-				workSheet.Cells[rowIndex, 1].Value.ToString(),
-				workSheet.Cells[rowIndex, 2].Value.ToString());
+				workSheet.Cells[rowIndex, 6].Value.ToString(),
+				workSheet.Cells[rowIndex, 7].Value.ToString(),
+				workSheet.Cells[rowIndex, 8].Value.ToString(),
+				workSheet.Cells[rowIndex, 9].Value.ToString(),
+				workSheet.Cells[rowIndex, 10].Value.ToString());
 		}
 	}
 }
