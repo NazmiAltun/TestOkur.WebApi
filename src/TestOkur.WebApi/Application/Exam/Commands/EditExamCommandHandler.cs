3945e1604e2b34140133c7eeef94e5b046044bd9
NAZMI <NAZMI@DESKTOP-20STISF>
2019-08-04 18:32:39 +0300

    Use Query object to check if exam exists

--- src/TestOkur.WebApi/Application/Exam/Commands/EditExamCommandHandler.cs
+++ src/TestOkur.WebApi/Application/Exam/Commands/EditExamCommandHandler.cs
@@ -1,33 +1,39 @@
 ﻿namespace TestOkur.WebApi.Application.Exam.Commands
 {
-    using System;
-    using System.ComponentModel.DataAnnotations;
-    using System.Linq;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using MassTransit;
-    using Microsoft.EntityFrameworkCore;
-    using Paramore.Brighter;
-    using TestOkur.Common;
-    using TestOkur.Data;
-    using TestOkur.Domain.Model.ExamModel;
-    using TestOkur.Domain.SeedWork;
-    using TestOkur.Infrastructure.Cqrs;
-    using Exam = TestOkur.Domain.Model.ExamModel.Exam;
-    using Lesson = TestOkur.Domain.Model.LessonModel.Lesson;
+	using System;
+	using System.ComponentModel.DataAnnotations;
+	using System.Linq;
+	using System.Threading;
+	using System.Threading.Tasks;
+	using MassTransit;
+	using Microsoft.EntityFrameworkCore;
+	using Paramore.Brighter;
+	using Paramore.Darker;
+	using TestOkur.Common;
+	using TestOkur.Data;
+	using TestOkur.Domain.Model.ExamModel;
+	using TestOkur.Domain.SeedWork;
+	using TestOkur.Infrastructure.Cqrs;
+	using TestOkur.WebApi.Application.Exam.Queries;
 
-    public sealed class EditExamCommandHandler
+	using Exam = TestOkur.Domain.Model.ExamModel.Exam;
+	using Lesson = TestOkur.Domain.Model.LessonModel.Lesson;
+
+	public sealed class EditExamCommandHandler
 		: RequestHandlerAsync<EditExamCommand>
 	{
+		private readonly IQueryProcessor _queryProcessor;
 		private readonly ApplicationDbContext _dbContext;
 		private readonly IPublishEndpoint _publishEndpoint;
 
 		public EditExamCommandHandler(
 			ApplicationDbContext dbContext,
-			IPublishEndpoint publishEndpoint)
+			IPublishEndpoint publishEndpoint,
+			IQueryProcessor queryProcessor)
 		{
 			_dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
 			_publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
+			_queryProcessor = queryProcessor;
 		}
 
 		[Idempotent(1)]
@@ -97,11 +103,11 @@
 			EditExamCommand command,
 			CancellationToken cancellationToken)
 		{
-			if (await _dbContext.Exams.AnyAsync(
-				c => c.Name == command.NewName &&
-					 c.Id != command.ExamId &&
-					 EF.Property<int>(c, "CreatedBy") == command.UserId,
-				cancellationToken))
+			var list = (await _queryProcessor.ExecuteAsync(
+				new GetUserExamsQuery(), cancellationToken)).ToList();
+
+			if (list.Any(c => c.Name == command.NewName &&
+					 c.Id != command.ExamId))
 			{
 				throw new ValidationException(ErrorCodes.ExamExists);
 			}
