3bb8475bcf5d66b31ecbe41007989286e34efa2e
NAZMI <NAZMI@DESKTOP-20STISF>
2019-08-12 11:08:08 +0300

    deployment script reworked

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 docker stop testokur-webapi && docker rm testokur-webapi
 docker run -d -e "ASPNETCORE_ENVIRONMENT=prod" \
-	-e VIRTUAL_HOST=webapi.testokur.com \
-	-e VIRTUAL_PORT=80 \
-	-e LETSENCRYPT_HOST=webapi.testokur.com \
-	-e LETSENCRYPT_EMAIL=bilgi@testokur.com \
+	--env-file  /home/docker-images/webapi-prod.env \
 	--name testokur-webapi \
 	--restart=always  \
 	--network=testokur \
@@ -13,6 +10,7 @@ docker run -d -e "ASPNETCORE_ENVIRONMENT=prod" \
 
 docker stop testokur-notification && docker rm testokur-notification
 docker run -d -e "ASPNETCORE_ENVIRONMENT=prod" \
+	--env-file  /home/docker-images/notification-prod.env \
 	--name testokur-notification \
 	--restart=always  \
 	--network=testokur \
@@ -21,10 +19,7 @@ docker run -d -e "ASPNETCORE_ENVIRONMENT=prod" \
 
 docker stop testokur-report && docker rm testokur-report
 docker run -d -e "ASPNETCORE_ENVIRONMENT=prod" \
-	-e VIRTUAL_HOST=report.testokur.com \
-	-e VIRTUAL_PORT=80 \
-	-e LETSENCRYPT_HOST=report.testokur.com \
-	-e LETSENCRYPT_EMAIL=bilgi@testokur.com \
+	--env-file  /home/docker-images/report-prod.env \
 	--name testokur-report \
 	--restart=always  \
 	--network=testokur \
--- deploy-webapi-qa.sh
+++ deploy-webapi-qa.sh
@@ -3,10 +3,6 @@ docker load -i /home/docker-images/webapi.tar
 docker stop testokur-webapi-qa && docker rm testokur-webapi-qa
 docker run -d \
 	--env-file  /home/docker-images/webapi-qa.env \
-	-e VIRTUAL_HOST=webapi-qa.testokur.com \
-	-e VIRTUAL_PORT=80 \
-	-e LETSENCRYPT_HOST=webapi-qa.testokur.com \
-	-e LETSENCRYPT_EMAIL=bilgi@testokur.com \
 	-v /home/data-qa:/app/Data \
 	-v /home/uploads-qa:/app/wwwroot/uploads \
 	--name testokur-webapi-qa \
