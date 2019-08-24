08cac8e17b432aa31cbb6be8747962fe632adceb
NAZMI <NAZMI@DESKTOP-20STISF>
2019-08-24 23:09:07 +0300

    some missing prod config added

--- deploy/webapi-prod.sh
+++ deploy/webapi-prod.sh
@@ -2,6 +2,8 @@
 docker stop testokur-webapi && docker rm testokur-webapi
 docker run -d \
 	--env-file /home/docker-images/webapi-prod.env \
+	-v /home/data:/app/Data \
+	-v /home/uploads:/app/wwwroot/uploads \
 	--name testokur-webapi \
 	--restart=unless-stopped \
 	--network=testokur \
