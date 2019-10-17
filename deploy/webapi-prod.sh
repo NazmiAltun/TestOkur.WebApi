e0fc24387ddc1c4e941e700310218e8ecf36882d
Nazmi Altun <nazmialtun@windowslive.com>
2019-10-17 08:32:01 +0800

    need 2 container on prod for webapi service

--- deploy/webapi-prod.sh
+++ deploy/webapi-prod.sh
@@ -4,9 +4,21 @@ docker stop testokur-webapi && docker rm testokur-webapi --force
 docker run -d \
 	--env-file /home/env/webapi-prod.env \
 	-v /home/uploads:/app/wwwroot/uploads \
+	-v /home/DataProtection-Keys:/app/DataProtection-Keys \
 	--name testokur-webapi \
 	--restart=unless-stopped \
 	--network=testokur \
 	--network-alias=webapi \
-	-m=512M \
+	-m=256M \
+	nazmialtun/testokur-webapi:latest
+docker stop testokur-webapi_2 && docker rm testokur-webapi_2 --force
+docker run -d \
+	--env-file /home/env/webapi-prod.env \
+	-v /home/uploads:/app/wwwroot/uploads \
+	-v /home/DataProtection-Keys:/app/DataProtection-Keys \
+	--name testokur-webapi_2 \
+	--restart=unless-stopped \
+	--network=testokur \
+	--network-alias=webapi_2 \
+	-m=256M \
 	nazmialtun/testokur-webapi:latest
