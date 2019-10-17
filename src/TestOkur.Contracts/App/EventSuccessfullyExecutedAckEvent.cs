f57a8aaaca74fb4e0955b1239cca5ac5acb7ae75
Nazmi Altun <nazmialtun@windowslive.com>
2019-10-17 10:21:52 +0800

    IEventSuccessfullyExecutedAckEvent interface and class for keeping track of event status

---
+++ src/TestOkur.Contracts/App/EventSuccessfullyExecutedAckEvent.cs
@@ -0,0 +1,17 @@
+﻿namespace TestOkur.Contracts.App
+{
+    using System;
+
+    public class EventSuccessfullyExecutedAckEvent : IntegrationEvent, IEventSuccessfullyExecutedAckEvent
+    {
+        public EventSuccessfullyExecutedAckEvent(Guid executedEventId, DateTime executedOnUtc)
+        {
+            ExecutedEventId = executedEventId;
+            ExecutedOnUtc = executedOnUtc;
+        }
+
+        public Guid ExecutedEventId { get; set; }
+
+        public DateTime ExecutedOnUtc { get; set; }
+    }
+}
---
+++ src/TestOkur.Contracts/App/IEventSuccessfullyExecutedAckEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TestOkur.Contracts.App
+{
+    public interface IEventSuccessfullyExecutedAckEvent : IIntegrationEvent
+    {
+        Guid ExecutedEventId { get; }
+
+        DateTime ExecutedOnUtc { get; }
+    }
+}
