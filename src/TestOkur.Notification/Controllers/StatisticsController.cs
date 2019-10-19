e11c70779365ae6930d817b845726a9a95d74e7a
Nazmi Altun <nazmialtun@windowslive.com>
2019-10-19 17:17:36 +0800

    Statistics endpoint added to notification API

---
+++ src/TestOkur.Notification/Controllers/StatisticsController.cs
@@ -0,0 +1,29 @@
+﻿namespace TestOkur.Notification.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+    using TestOkur.Common;
+    using TestOkur.Notification.Dtos;
+    using TestOkur.Notification.Infrastructure.Data;
+
+    [Route("api/v1/statistics")]
+    [Authorize(AuthorizationPolicies.Admin)]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IStatsRepository _statsRepository;
+
+        public StatisticsController(IStatsRepository statsRepository)
+        {
+            _statsRepository = statsRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(NotificationStatisticsDto), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _statsRepository.GetStatisticsAsync());
+        }
+    }
+}
