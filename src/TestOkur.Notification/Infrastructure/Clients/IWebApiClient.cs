3506087826129d80ba4533cc49a2809da0c33500
Nazmi Altun <nazmialtun@windowslive.com>
2019-10-20 21:25:51 +0800

    DeductSmsCreditsAsync method marked obsolete

--- src/TestOkur.Notification/Infrastructure/Clients/IWebApiClient.cs
+++ src/TestOkur.Notification/Infrastructure/Clients/IWebApiClient.cs
@@ -1,5 +1,6 @@
 ﻿namespace TestOkur.Notification.Infrastructure.Clients
 {
+    using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using TestOkur.Notification.Models;
@@ -10,6 +11,7 @@
 
         Task<AppSettingReadModel> GetAppSettingAsync(string name);
 
+        [Obsolete("This will be removed in future released. Better publish an event rather than making an http call to the api")]
         Task DeductSmsCreditsAsync(int userId, string smsBody);
 
         Task<StatisticsReadModel> GetStatisticsAsync();
