2242e24e9f76cb1b9a8473862af8fb1fb8444332
Nazmi Altun <nazmialtun@windowslive.com>
2019-12-05 00:14:10 +0800

    ITestOutputHelper added to HealthCheckTests

--- tests/TestOkur.Notification.Integration.Tests/HealthCheckTests.cs
+++ tests/TestOkur.Notification.Integration.Tests/HealthCheckTests.cs
@@ -5,20 +5,24 @@
     using FluentAssertions;
     using Microsoft.AspNetCore.Mvc.Testing;
     using Xunit;
+    using Xunit.Abstractions;
 
     public class HealthCheckTests : IClassFixture<WebApplicationFactory<Startup>>
     {
         private readonly WebApplicationFactory<Startup> _factory;
+        private readonly ITestOutputHelper _testOutputHelper;
 
-        public HealthCheckTests(WebApplicationFactory<Startup> factory)
+        public HealthCheckTests(WebApplicationFactory<Startup> factory, ITestOutputHelper testOutputHelper)
         {
             _factory = factory;
+            _testOutputHelper = testOutputHelper;
         }
 
         [Fact]
         public async Task HealthCheckEndpointShouldWork_WhenServerIsRunning()
         {
             var response = await _factory.CreateClient().GetAsync("hc");
+            _testOutputHelper.WriteLine(await response.Content.ReadAsStringAsync());
             response.StatusCode.Should().Be(HttpStatusCode.OK);
             response.Content.Headers.ContentType.MediaType.Should().Be("application/json");
         }
