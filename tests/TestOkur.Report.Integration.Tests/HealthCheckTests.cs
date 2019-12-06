6b17c850ed3c65b0a09f5c27d83c63d12bab1fd9
Nazmi Altun <nazmialtun@windowslive.com>
2019-12-06 18:40:31 +0800

    ITestOutputHelper added to HealthCheckEndpointShouldWork_WhenServerIsRunning

--- tests/TestOkur.Report.Integration.Tests/HealthCheckTests.cs
+++ tests/TestOkur.Report.Integration.Tests/HealthCheckTests.cs
@@ -1,17 +1,26 @@
 ﻿namespace TestOkur.Report.Integration.Tests
 {
+    using FluentAssertions;
     using System.Net;
     using System.Threading.Tasks;
-    using FluentAssertions;
     using TestOkur.Report.Integration.Tests.Common;
     using Xunit;
+    using Xunit.Abstractions;
 
     public class HealthCheckTests
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public HealthCheckTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public async Task HealthCheckEndpointShouldWork_WhenServerIsRunning()
         {
             var response = await TestServerFactory.TestServer.CreateClient().GetAsync("hc");
+            _testOutputHelper.WriteLine(await response.Content.ReadAsStringAsync());
             response.StatusCode.Should().Be(HttpStatusCode.OK);
             response.Content.Headers.ContentType.MediaType.Should().Be("application/json");
         }
