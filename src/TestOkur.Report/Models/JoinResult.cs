b80b21f1c231953330173947f2667239fbf1df1a
Nazmi Altun <nazmialtun@windowslive.com>
2020-01-27 22:56:49 +0800

    Join and evaluate contract change

---
+++ src/TestOkur.Report/Models/JoinResult.cs
@@ -0,0 +1,13 @@
+﻿namespace TestOkur.Report.Models
+{
+    using System.Collections.Generic;
+    using TestOkur.Optic.Form;
+    using TestOkur.Report.Domain.Statistics;
+
+    public class JoinResult
+    {
+        public ExamStatistics ExamStatistics { get; set; }
+
+        public List<StudentOpticalForm> Forms { get; set; }
+    }
+}
