4ffd0b159616ec95e8ef7d3c28ca4a5cc92fa4b8
Nazmi Altun <nazmialtun@windowslive.com>
2020-02-10 14:34:46 +0800

    Annotation for RabbitMqConfiguration

--- src/TestOkur.Common/Configuration/RabbitMqConfiguration.cs
+++ src/TestOkur.Common/Configuration/RabbitMqConfiguration.cs
@@ -1,13 +1,19 @@
 ﻿namespace TestOkur.Common.Configuration
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class RabbitMqConfiguration
     {
+        [Required]
         public string Uri { get; set; }
 
+        [Required]
         public string Vhost { get; set; }
 
+        [Required]
         public string Username { get; set; }
 
+        [Required]
         public string Password { get; set; }
     }
 }
