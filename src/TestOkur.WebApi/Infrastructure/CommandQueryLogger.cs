e6c9ffa13ccb1553e14e5207e4d272d5f8e020ad
Nazmi Altun <nazmialtun@windowslive.com>
2020-03-07 13:17:16 +0800

    Add user id to command logs

--- src/TestOkur.WebApi/Infrastructure/CommandQueryLogger.cs
+++ src/TestOkur.WebApi/Infrastructure/CommandQueryLogger.cs
@@ -11,25 +11,27 @@
     public class CommandQueryLogger : ICommandQueryLogger
     {
         private readonly ILogger<CommandQueryLogger> _logger;
+        private readonly IUserIdProvider _userIdProvider;
 
-        public CommandQueryLogger(ILogger<CommandQueryLogger> logger)
+        public CommandQueryLogger(ILogger<CommandQueryLogger> logger, IUserIdProvider userIdProvider)
         {
             _logger = logger;
+            _userIdProvider = userIdProvider;
         }
 
         public Task LogQueryAsync<TQuery>(TQuery query)
             where TQuery : IQuery
         {
-            try
-            {
-                var type = query.GetType().ToString();
-                var queryContent = JsonUtils.Serialize(query);
-                _logger.LogWarning("Query. Type : {Type} - {QueryContent}", type, queryContent);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error occured while logging query");
-            }
+            //try
+            //{
+            //    var type = query.GetType().ToString();
+            //    var queryContent = JsonUtils.Serialize(query);
+            //    _logger.LogWarning("Query. Type : {Type} - {QueryContent}", type, queryContent);
+            //}
+            //catch (Exception ex)
+            //{
+            //    _logger.LogError(ex, "Error occured while logging query");
+            //}
 
             return Task.CompletedTask;
         }
@@ -37,34 +39,33 @@
         public void LogQuery<TQuery>(TQuery query)
             where TQuery : IQuery
         {
-            try
-            {
-                var type = query.GetType().ToString();
-                var queryContent = JsonUtils.Serialize(query);
-                _logger.LogWarning("Query. Type : {Type} - {QueryContent}", type, queryContent);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error occured while logging query");
-            }
+            //try
+            //{
+            //    var type = query.GetType().ToString();
+            //    var queryContent = JsonUtils.Serialize(query);
+            //    _logger.LogWarning("Query. Type : {Type} - {QueryContent}", type, queryContent);
+            //}
+            //catch (Exception ex)
+            //{
+            //    _logger.LogError(ex, "Error occured while logging query");
+            //}
         }
 
-        public Task LogCommandAsync<TCommand>(TCommand command)
+        public async Task LogCommandAsync<TCommand>(TCommand command)
             where TCommand : IRequest
         {
             try
             {
                 var type = command.GetType().ToString();
                 var commandContent = JsonUtils.Serialize(command);
+                var userId = await _userIdProvider.GetAsync();
                 _logger.LogWarning(
-                    "Command. Type : {Type} - {CommandContent}", type, commandContent);
+                    "Command. UserId : {userId}. Type : {Type} - {CommandContent}", userId, type, commandContent);
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Error occured while logging query");
             }
-
-            return Task.CompletedTask;
         }
 
         public void LogCommand<TCommand>(TCommand command)
@@ -74,8 +75,10 @@
             {
                 var type = command.GetType().ToString();
                 var commandContent = JsonUtils.Serialize(command);
+                var userId = _userIdProvider.Get();
+
                 _logger.LogWarning(
-                    "Command. Type : {Type} - {CommandContent}", type, commandContent);
+                    "Command. UserId : {userId}. Type : {Type} - {CommandContent}", userId, type, commandContent);
             }
             catch (Exception ex)
             {
